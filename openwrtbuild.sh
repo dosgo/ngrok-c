f995070ab89d8c970620feaa07cf48fc45acd8a8
luoshishan <luoshishan@mofox.cn>
2015-10-17 16:08:15 +0800

    .

--- openwrtbuild.sh
+++ openwrtbuild.sh
@@ -17,7 +17,8 @@ $CC $YH -c $1 cJSON.c -o $DIR/cJSON.o
 $CC $YH -c $1 main.cpp -o $DIR/main.o
 $CC $YH -c $1 nonblocking.cpp  -o $DIR/nonblocking.o
 $CC $YH -c $1 sslbio.cpp  -o $DIR/sslbio.o
-$CC -s $DIR/main.o $DIR/cJSON.o $DIR/sendmsg.o $DIR/nonblocking.o  $DIR/sslbio.o libpolarssl-mips.a -o $DIR/ngrokc   -lpthread
+$CC $YH -c $1 ngrok.cpp  -o $DIR/ngrok.o
+$CC -s $DIR/main.o $DIR/cJSON.o $DIR/sendmsg.o $DIR/nonblocking.o $DIR/ngrok.o $DIR/sslbio.o libpolarssl-mips.a -o $DIR/ngrokc   -lpthread
 
 #buill openssl
 #CC=mips-openwrt-linux-gcc
