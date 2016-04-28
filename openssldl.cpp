87ed89e0c2522d91959327e54f87994ae86a835f
dosgo <dosgo@qq.com>
2016-04-28 23:39:30 +0800

    .

--- openssldl.cpp
+++ openssldl.cpp
@@ -46,6 +46,9 @@ TSslX509d2i          SslX509d2i = 0;
 TSslX509i2d          SslX509i2d = 0;
 TSslSetConnectState  SslSetConnectState = 0;//dosgo
 TSslDoHandshake  SslDoHandshake = 0;//dosgo
+TSsLGetSession   SsLGetSession = 0;//dosgo
+TSsLSetSession   SsLSetSession = 0;//dosgo
+
 
 //----------------------------------------------------------------------------
 void FechaSSL()
@@ -127,6 +130,8 @@ const char * AbreSSL()
     SslX509i2d          = (TSslX509i2d)    GETPROC(ssl_handle2, "i2d_X509");
     SslSetConnectState  = (TSslSetConnectState)   GETPROC(ssl_handle1, "SSL_set_connect_state");//dosgo
     SslDoHandshake      = (TSslDoHandshake)   GETPROC(ssl_handle1, "SSL_do_handshake");//dosgo
+    SsLGetSession      = (TSsLGetSession)   GETPROC(ssl_handle1, "SSL_get_session");//dosgo
+    SsLSetSession      = (TSsLSetSession)   GETPROC(ssl_handle1, "SSL_set_session");//dosgo
     const char * erro = 0;
     if (!SslGetError)         erro = "SSL_get_error";
     if (!SslLibraryInit)      erro = "SSL_library_init";
@@ -155,6 +160,8 @@ const char * AbreSSL()
     if (!SslX509i2d)          erro = "i2d_X509";
     if (!SslSetConnectState)  erro = "SSL_set_connect_state";
     if (!SslDoHandshake)     erro = "SSL_do_handshake";
+    if (!SsLGetSession)     erro = "SSL_get_session";
+    if (!SsLSetSession)     erro = "SSL_set_session";
     if (erro)
     {
         FechaSSL();
--- openssldl.h
+++ openssldl.h
@@ -55,6 +55,8 @@ typedef X509 *  (*TSslX509d2i)(X509 **px, const unsigned char **in, int len);
 typedef int     (*TSslX509i2d)(X509 *x, unsigned char **out);
 typedef void    (*TSslSetConnectState)(SSL *ssl);//SSL_set_connect_state dosgo
 typedef int     (*TSslDoHandshake)(SSL *ssl);//SSL_do_handshake dosgo
+typedef SSL_SESSION * (*TSsLGetSession)(const SSL *ssl);//SSL_get_session dosgo
+typedef int     (*TSsLSetSession)(SSL *to, SSL_SESSION *session);//SSL_set_session dosgo
 //----------------------------------------------------------------------------
 // Ponteiros de funções
 extern TSslGetError         SslGetError;
@@ -85,6 +87,8 @@ extern TSslX509d2i          SslX509d2i;
 extern TSslX509i2d          SslX509i2d;
 extern TSslSetConnectState  SslSetConnectState;  //dosgo
 extern TSslDoHandshake      SslDoHandshake;  //dosgo
+extern TSsLSetSession      SsLSetSession;  //dosgo
+extern TSsLGetSession      SsLGetSession;  //dosgo
 //----------------------------------------------------------------------------
 
 
