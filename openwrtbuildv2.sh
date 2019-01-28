264cf5d32a84067f4eb6528f8e462e6031fb507d
dosg <dosgo@qq.com>
2019-01-28 18:04:11 +0800

    fix linux

--- openwrtbuildv2.sh
+++ openwrtbuildv2.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-export PATH=$PATH:'/root/ledesdk/staging_dir/toolchain-mips_24kc_gcc-5.4.0_musl-1.1.16/bin'
-export STAGING_DIR="/root/ledesdk/staging_dir"
+export PATH=$PATH:'/root/mt76x8sdk/staging_dir/toolchain-mipsel_24kc_gcc-7.3.0_musl/bin'
+export STAGING_DIR="/root/mt76x8sdk/staging_dir"
+
 DIR=build-mips
 mkdir $DIR
 rm -rf $DIR/main.o
@@ -10,9 +11,10 @@ rm -rf $DIR/polarsslbio.o
 rm -rf $DIR/nonblocking.o
 rm -rf $DIR/ngrokc
 
-CC=mips-openwrt-linux-g++
+CC=mipsel-openwrt-linux-g++
 YH="-Wall -fexceptions -DOPENSSL=1 -DOPENSSLDL=1   -O2"
 $CC $YH -c $1 sendmsg.cpp -o $DIR/sendmsg.o
+$CC $YH -c $1 udp.cpp -o $DIR/udp.o
 $CC $YH -c $1 cJSON.c -o $DIR/cJSON.o
 $CC $YH -c $1 main.cpp -o $DIR/main.o
 $CC $YH -c $1 nonblocking.cpp  -o $DIR/nonblocking.o
@@ -20,5 +22,5 @@ $CC $YH -c $1 opensslbio.cpp  -o $DIR/opensslbio.o
 $CC $YH -c $1 openssldl.cpp  -o $DIR/openssldl.o
 $CC $YH -c $1 global.cpp  -o $DIR/global.o
 $CC $YH -c $1 ngrok.cpp  -o $DIR/ngrok.o
-$CC -s $DIR/main.o $DIR/cJSON.o $DIR/sendmsg.o $DIR/nonblocking.o $DIR/global.o $DIR/opensslbio.o  $DIR/ngrok.o $DIR/openssldl.o    -o $DIR/ngrokc
+$CC -s $DIR/main.o $DIR/cJSON.o $DIR/sendmsg.o $DIR/nonblocking.o $DIR/global.o $DIR/opensslbio.o  $DIR/ngrok.o $DIR/openssldl.o  $DIR/udp.o   -o $DIR/ngrokc
 
