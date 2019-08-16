6e699b14f3395b10500e18ce2efd8a7979307972
onlyJinx <925198583@qq.com>
2019-08-16 12:58:28 +0800

    Update buildlib.sh

--- buildlib.sh
+++ buildlib.sh
@@ -1,8 +1,10 @@
 #this build polarssl
-export PATH=$PATH:'/home/dosgo/OpenWrt-SDK/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin'
-export STAGING_DIR="/home/dosgo/OpenWrt-SDK/staging_dir"
+###修改成SDK路径
+export PATH=$PATH:'/root/op/staging_dir/toolchain-mipsel_1004kc+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin'
+export STAGING_DIR="/root/op/staging_dir"
 make clean
-make CC=mips-openwrt-linux-gcc CXX=mips-openwrt-linux-g++ AR=mips-openwrt-linux-ar RANLIB=mips-openwrt-linux-ranlib
+###修改成SDK编译器
+make CC=mipsel-openwrt-linux-uclibc-gcc CXX=mipsel-openwrt-linux-uclibc-g++ AR=mipsel-openwrt-linux-uclibc-ar RANLIB=mipsel-openwrt-linux-uclibc-ranlib
 
 
 
